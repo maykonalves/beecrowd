53d8635cede392f547dc877fe4906b7c22678768
Maykon <Maykon98@live.com>
2022-09-15 18:23:06 -0300

    15/09

---
+++ 1000/1008.py
@@ -0,0 +1,8 @@
+number = int(input())
+workedHours = int(input())
+hourlyWage = float(input())
+
+salary = workedHours*hourlyWage
+
+print("NUMBER = {}".format(number))
+print("SALARY = U$ %0.2f" %salary)
